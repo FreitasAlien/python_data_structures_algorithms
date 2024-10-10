7e5c5f708852681cd5cd4499e4b5c01eb25c9261
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-10-10 16:52:31 -0300

    Numeric Types

---
+++ Chapter01/numeric_types.py
@@ -0,0 +1,12 @@
+#Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+i = 45   # int
+
+#Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+f = 2.8  # float
+
+#Complex numbers are written with a "j" as the imaginary part:  
+c = 4 + 1j   # complex
+
+print(type(i))
+print(type(f))
+print(type(c))
